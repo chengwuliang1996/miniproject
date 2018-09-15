8f891f935442bd253666872814b752061aaf9e4a
Wuliang Cheng <cwl@bu.edu>
2018-09-15 15:10:10 -0400

    miniproject

---
+++ GoogleAuth/GoogleAuth/Controllers/ChartsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Charts.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Charts.Controllers
+{
+    public class ChartsController : Controller
+    {
+        public IActionResult Line()
+        {
+            string t;
+            string h;
+            int i = 0;
+            var lstModel = new List<SimpleReportViewModel>();
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Chengwuliang\Desktop\w\test\GoogleAuth\GoogleAuth\Views\Home\Temp.txt");
+            System.IO.StreamReader file2 = new System.IO.StreamReader(@"C:\Users\Chengwuliang\Desktop\w\test\GoogleAuth\GoogleAuth\Views\Home\Humidity.txt");
+
+            while (((t = file.ReadLine()) != null) && ((h = file2.ReadLine()) != null))
+            {
+                lstModel.Add(new SimpleReportViewModel
+                {
+                    Day = i,
+                    Temp = Convert.ToInt32(t),
+                    Humidity = Convert.ToInt32(h)
+                });
+                i++;
+            }
+            return View( lstModel );
+        }
+    }
+}
